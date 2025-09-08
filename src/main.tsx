439d3c7717ff0a9c99cee0bd49f68440d3d01cf8
vlad <infinitiesair@gmail.com>
2025-09-08 22:40:53 +0300

    task complited

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'modern-normalize/modern-normalize.css';
+
+import './index.css';
+import App from './components/App/App';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <>
+      <App />
+      <Toaster position="top-right" />
+    </>
+  </StrictMode>
+);
+
