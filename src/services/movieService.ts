4ad24af1e2ce01c297dd919c906d8d04b8b349e4
vlad <infinitiesair@gmail.com>
2025-09-12 18:16:26 +0300

    bug fixes 4

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
-import type { Movie } from "../types/movie";
+import axios from 'axios';
+import type { Movie } from '../types/movie';
 
-const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
 interface FetchMoviesParams {
   query: string;
@@ -11,15 +11,13 @@ interface TMDBResponse {
   results: Movie[];
 }
 
-export const fetchMovies = async ({
-  query,
-}: FetchMoviesParams): Promise<TMDBResponse> => {
+export const fetchMovies = async ({ query }: FetchMoviesParams): Promise<TMDBResponse> => {
   const response = await axios.get<TMDBResponse>(BASE_URL, {
     params: {
       query,
     },
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWRmODI2NmNmZjkxZGMxZGFmZjc0NDUzMTQyMDc3MCIsIm5iZiI6MTc1NzY4NjQ2MS44NDMwMDAyLCJzdWIiOiI2OGM0MmFiZGU3NDc0NDJhZTgyYTA4YzkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.NEHPJBU9jAo_FYCzNvxvA3ySat0lS9BjdboWXD39ZhI`,
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
   });
 
